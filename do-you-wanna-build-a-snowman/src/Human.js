d117c9861888071e2cf47d3fa661aed714d3b864
Nicole Forseth <forseth.nico@gmail.com>
2022-05-23 12:13:42 -0600

    Initial commit

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,25 @@
 var Snowman = require('./Snowman');
 
 class Human {
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    }
+    gatherMaterials(material, number) {
+      this.materials[material] = this.materials[material] + number;
+    }
 
+    buildASnowman() {
+//create new instance of Snowman class
+      var snowman = new Snowman(this.materials)
+      return snowman;
+    }
+  }
 }
 
 module.exports = Human;
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,4 +1,16 @@
 class Snowman {
+  constructor(snowManDetails) {
+    this.carrots = snowManDetails.carrots
+    this.coal = snowManDetails.coal
+    this.buttons = snowManDetails.buttons
+    this.snowballs = snowManDetails.snowballs
+    this.magicHat = false
+  }
+  canWearMagicHat() {
+    if (this.coal >= 2 && this.buttons >= 5 && this.carrots >= 1 && this.snowballs >= 2) {
+      this.magicHat = true;
+    }
+  }
 
 }
 
