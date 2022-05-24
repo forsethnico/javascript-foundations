1054da3195d6b3b506a23cca8dcf7708b166a3b7
Nicole Forseth <forseth.nico@gmail.com>
2022-05-23 22:47:31 -0600

    Add implementation code for all passing pirate tests

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,33 @@
+class Pirate {
+  constructor(name, job) {
+    this.name = name;
+    this.job = job || 'scallywag';
+    this.cursed = false;
+    this.booty = 0;
+  }
+  robShip() {
+    if (this.booty === 500) {
+      this.cursed = true;
+      return "ARG! I've been cursed!"
+    } else {
+    this.booty += 100;
+    return "YAARRR!";
+    }
+  }
+  liftCurse() {
+    if (this.booty === 500) {
+      this.booty = 200;
+      this.cursed = false;
+      return "Your curse has been lifted!"
+    } else if (this.booty < 500) {
+      return "You don't need to lift a curse!"
+    }
+  }
+}
+
+
+
+
+
+
+module.exports = Pirate;
