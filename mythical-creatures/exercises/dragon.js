fc8750a2d4cd5a9065ffaf134ed67d255dedb8f6
Nicole Forseth <forseth.nico@gmail.com>
2022-05-23 20:41:04 -0600

    Add implementation code to make all dragon test pass

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+var eatCounter = 0;
 
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
+  greet(greeting) {
+    return `Hi, ${this.rider}!`
+  }
+  eat() {
+    eatCounter ++
+    if (eatCounter >= 3){
+      return this.hungry = false;
+    }
+  }
+}
+
+
+module.exports = Dragon;
