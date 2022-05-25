30b88d741e451c18a7a45cc1feff510c8d18e0f0
Nicole Forseth <forseth.nico@gmail.com>
2022-05-24 23:25:37 -0600

    Add implementation code to make all Medusa tests pass

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,23 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    var victimStatue = new Statue(victim.name)
+      if (this.statues.length < 3) {
+        this.statues.push(victimStatue);
+      } else if (this.statues.length === 3) {
+        var alivePerson = this.statues.shift();
+        this.statues.push(victimStatue);
+        var revivedPerson = new Person(alivePerson.name)
+        revivedPerson.mood = "relieved";
+        return revivedPerson;
+      }
+  }
+}
+
+module.exports = Medusa;
