f3a4b3fc40ee6165331d32ace5cb85ea6dc404a0
Nicole Forseth <forseth.nico@gmail.com>
2022-05-25 22:19:37 -0600

    Add implementation code to pass all magician tests

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,32 @@
+class Magician {
+  constructor(magicPerson) {
+    this.name = `The Great ${magicPerson.name}`
+    this.assistant = magicPerson.assistant;
+    this.favoriteAccessory = magicPerson.clothing || "top hat";
+    this.confidencePercentage = 10;
+    }
+  performIncantation(magic) {
+    return `${magic.toUpperCase()}!`
+  }
+  performTrick() {
+    this.confidencePercentage += 10;
+    if (this.favoriteAccessory === "top hat") {
+      return "PULL RABBIT FROM TOP HAT";
+      } else {
+      return "PULL DOVE FROM SLEEVE";
+    }
+  }
+  performShowStopper () {
+    if (this.confidencePercentage >= 100 && this.assistant == true) {
+      return 'WOW! The magician totally just sawed that person in half!';
+    } else {
+      return 'Oh no, this trick is not ready!'
+    }
+  }
+}
+
+
+
+
+
+module.exports = Magician;
